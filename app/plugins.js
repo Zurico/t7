ccf3e26c090e4e389ea47d4ed4e01b48a64fe85d
Zurisadai Pabon <zpabon@itrsgroup.com>
2017-01-27 10:54:29 +0100

    updated plugin file

--- app/plugins.js
+++ app/plugins.js
@@ -10,3 +10,23 @@ import setupNotebookGraph from 'containers/NotebookGraph/setup';
 setupNotebookJS(Register);
 setupNotebookTalo(Register);
 setupNotebookGraph(Register);
+
+// Desktop production workflow:
+// Download NotebookJS folder to ~/.talo/packages
+// On init startup (this file) loop all folders in ~/.talo/packages and foreach
+// require(system.import) them and pass the Register (this should be an api for passing other modules
+// as well), the required package should be ready for production (a minified build)
+
+// Desktop dev workflow:
+// Download NotebookJS folder to ~/.talo/dev-packages
+// On init startup (this file) loop all folders in ~/.talo/dev-packages and foreach
+// require(system.import) them with babelify and pass the Register (this should be an api for passing other modules
+// as well)
+
+// Web production workflow:
+// On init startup (this file) loop all packages uris in user's/ system's packages config and foreach
+// require(system.import) them and pass the Register (this should be an api for passing other modules
+// as well), the required package should be ready for production (a minified build)
+
+// Web dev workflow:
+// Same workflow as creating a containers/X module (?)
